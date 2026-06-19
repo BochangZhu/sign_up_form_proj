c0753e860f6ab352310e9bc2b8021db236a7670a
BochangZhu <160828488+BochangZhu@users.noreply.github.com>
2026-06-18 18:10:26 -0700

    improve validation logic with regex patterns

--- control.js
+++ control.js
@@ -1,1 +1,24 @@
-const required = document.querySelectorAll("input[required]");
+const names = document.querySelectorAll("input.name");
+
+names.forEach((name)=>{
+    name.addEventListener("input", ()=>{
+        if (name.validity.patternMismatch) {
+            name.setCustomValidity("Name should only have letters!")
+        }
+        else {
+            name.setCustomValidity("");
+        }
+    })
+});
+
+
+
+const pass = document.querySelector("#password");
+pass.addEventListener("input",(e) =>{
+    if (pass.validity.patternMismatch){
+        pass.setCustomValidity("Password format not allowed.");
+    }
+    else {
+        pass.setCustomValidity("");
+    }
+})
